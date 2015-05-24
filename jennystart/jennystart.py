7864f29a3d2370397b40110fd3a3933eb3e7292e
yannizhang <jennyzhang8800@163.com>
2015-05-25 05:39:32 +0800

    add util.py

--- jennystart/jennystart.py
+++ jennystart/jennystart.py
@@ -1,3 +1,4 @@
+__author__ = 'zhangyanni'
 """TO-DO: Write a description of what this XBlock is."""
 
 import pkg_resources
@@ -5,6 +6,8 @@ import pkg_resources
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer
 from xblock.fragment import Fragment
+
+from lib_util import Util
 
 
 class JennystartXBlock(XBlock):
@@ -20,11 +23,8 @@ class JennystartXBlock(XBlock):
         default=0, scope=Scope.user_state,
         help="A simple counter, to show something happening",
         )
+    logger = Util .uc_logger()
 
-    def resource_string(self, path):
-        """Handy helper for getting resources from our kit."""
-        data = pkg_resources.resource_string(__name__, path)
-        return data.decode("utf8")
 
     # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
@@ -32,18 +32,18 @@ class JennystartXBlock(XBlock):
         The primary view of the JennystartXBlock, shown to students
         when viewing courses.
         """
-        html = self.resource_string("static/html/jennystart.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/jennystart.css"))
-        frag.add_css(self.resource_string("static/css/codemirror.css"))
-        frag.add_css(self.resource_string("static/css/night.css"))
-        frag.add_javascript(self.resource_string("static/js/src/jennystart.js"))
-        frag.add_javascript(self.resource_string("static/js/src/codemirror.js"))
-        frag.add_javascript(self.resource_string("static/js/src/active-line.js"))
-        frag.add_javascript(self.resource_string("static/js/src/clike.js"))
-        frag.add_javascript(self.resource_string("static/js/src/matchbrackets.js"))
-        frag.initialize_js('JennystartXBlock')
-        return frag
+        fragment = Fragment()
+        fragment.add_content(Util.load_resource("static/html/jennystart.html"))
+        fragment.add_css(Util.load_resource("static/css/jennystart.css"))
+        fragment.add_css(Util.load_resource("static/css/codemirror.css"))
+        fragment.add_css(Util.load_resource("static/css/night.css"))
+        fragment.add_javascript(Util.load_resource("static/js/src/jennystart.js"))
+        fragment.add_javascript(Util.load_resource("static/js/src/codemirror.js"))
+        fragment.add_javascript(Util.load_resource("static/js/src/active-line.js"))
+        fragment.add_javascript(Util.load_resource("static/js/src/clike.js"))
+        fragment.add_javascript(Util.load_resource("static/js/src/matchbrackets.js"))
+        fragment.initialize_js('JennystartXBlock')
+        return fragment
 
     # TO-DO: change this handler to perform your own actions.  You may need more
     # than one handler, or you may not need any handlers at all.
